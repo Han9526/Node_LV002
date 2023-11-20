5a7c0f2cbc4a68557849473d77161d81a8234d98
한승준 <jk2tmdwns@naver.com>
2023-11-20 16:45:15 +0900

    fix : PORT

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const usersRouter = require('./routers/signup.router');
 const authRouter = require('./routers/signin.router');
 const productRouter = require('./routers/products.router');
-
+require('dotenv').config();
+const env = process.env;
 const sequelize = require('./models').sequelize;
 
 const app = express();
-const PORT = 3000;
 
 sequelize.sync();
+
 app.use(express.json());
-app.use(cookieParser());
+
 app.use('/api', [usersRouter, authRouter, productRouter]);
 
-app.listen(PORT, () => {
-  console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
+app.listen(env.PORT, () => {
+  console.log(env.PORT, '포트 번호로 서버가 실행되었습니다.');
 });
