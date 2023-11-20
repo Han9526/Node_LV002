6c4c1afdab5134dfff2f22c081e0f99673d368b7
한승준 <jk2tmdwns@naver.com>
2023-11-20 16:46:05 +0900

    del : etc..

--- config/config.js
+++ config/config.js
@@ -9,18 +9,4 @@ module.exports = {
     host: env.DB_HOST,
     dialect: env.DB_TYPE,
   },
-  test: {
-    username: env.TEST_DB_USERNAME,
-    password: env.TEST_DB_PASSWORD,
-    database: env.TEST_DB_DATABASE,
-    host: env.TEST_DB_HOST,
-    dialect: env.TEST_DB_DIALECT,
-  },
-  production: {
-    username: env.PRODUCTION_DB_USERNAME,
-    password: env.PRODUCTION_DB_PASSWORD,
-    database: env.PRODUCTION_DB_DATABASE,
-    host: env.PRODUCTION_DB_HOST,
-    dialect: env.PRODUCTION_DB_DIALECT,
-  },
 };
